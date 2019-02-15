d7a6864f01037ffb8edf3bfe479bd7107c10155e
yury.tveritin <yury.tveritin@ardecs.ru>
2019-02-15 11:37:51 +0400

    it's  work for spring boot 2.0.1 and activiti 6.0.0  with MySQL

--- src/main/java/ru/MyApp.java
+++ src/main/java/ru/MyApp.java
@@ -1,31 +1,50 @@
 package ru;
 
 
+import javax.sql.DataSource;
+
+import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MyApp {
-
 
     public static void main(String[] args) {
         SpringApplication.run(MyApp.class, args);
     }
 
-    /*@Bean
-    CommandLineRunner init(final RepositoryService repositoryService,
-                           final RuntimeService runtimeService,
-                           final TaskService taskService) {
+    @Bean
+    public CommandLineRunner init(final RepositoryService repositoryService,
+                                  final RuntimeService runtimeService,
+                                  final TaskService taskService) {
 
         return new CommandLineRunner() {
-
+            @Override
             public void run(String... strings) throws Exception {
-                Map<String, Object> variables = new HashMap<String, Object>();
-                variables.put("applicantName", "John Doe");
-                variables.put("email", "john.doe@activiti.com");
-                variables.put("phoneNumber", "123456789");
-                runtimeService.startProcessInstanceByKey("hireProcess", variables);
+                System.out.println("Number of process definitions : "
+                        + repositoryService.createProcessDefinitionQuery().count());
+                System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
+                runtimeService.startProcessInstanceByKey("oneTaskProcess");
+                System.out.println("Number of tasks after process start: " + taskService.createTaskQuery().count());
             }
         };
-    }*/
+
+    }
+
+    @Bean
+    public DataSource database() {
+        return DataSourceBuilder.create()
+                .url("jdbc:mysql://127.0.0.1:3306/activiti-spring-boot?characterEncoding=UTF-8")
+                .username("root")
+                .password("123456")
+                .driverClassName("com.mysql.jdbc.Driver")
+                .build();
+    }
+
 }
