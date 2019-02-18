ae3b48e68d4bc162362acb84708245206439fffa
yury.tveritin <yury.tveritin@ardecs.ru>
2019-02-18 17:27:30 +0400

    it's  work for spring boot 2.0.1 and activiti 6.0.0  with MySQL

--- src/main/java/ru/MyApp.java
+++ src/main/java/ru/MyApp.java
@@ -3,9 +3,13 @@ package ru;
 
 import javax.sql.DataSource;
 
+import java.util.List;
+
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,6 +21,7 @@ public class MyApp {
 
     public static void main(String[] args) {
         SpringApplication.run(MyApp.class, args);
+
     }
 
     @Bean
@@ -27,11 +32,22 @@ public class MyApp {
         return new CommandLineRunner() {
             @Override
             public void run(String... strings) throws Exception {
-                System.out.println("Number of process definitions : "
-                        + repositoryService.createProcessDefinitionQuery().count());
+                System.out.println("Number of process definitions:" + repositoryService.createProcessDefinitionQuery().count());
+
                 System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
-                runtimeService.startProcessInstanceByKey("oneTaskProcess");
+
+                ProcessInstance processInstance1 = runtimeService.startProcessInstanceByKey("oneTaskProcess");
+                ProcessInstance processInstance2 = runtimeService.startProcessInstanceByKey("financialReport");
+
                 System.out.println("Number of tasks after process start: " + taskService.createTaskQuery().count());
+
+                List<Task> tasks = taskService.createTaskQuery().active().list();
+                for (Task task: tasks                     ) {
+                    taskService.complete(task.getId());
+
+                }
+
+                System.out.println(tasks.toString());
             }
         };
 
@@ -46,5 +62,15 @@ public class MyApp {
                 .driverClassName("com.mysql.jdbc.Driver")
                 .build();
     }
+
+//    @Bean
+//    public DataSource database() {
+//        return DataSourceBuilder.create()
+//                .url("jdbc:mysql://127.0.0.1:3306/activiti-spring-boot?characterEncoding=UTF-8")
+//                .username("root")
+//                .password("123456")
+//                .driverClassName("com.mysql.jdbc.Driver")
+//                .build();
+//    }
 
 }
